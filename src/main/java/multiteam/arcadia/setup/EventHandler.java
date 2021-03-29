9a11269a61d2271fe113d2486eda5e5990ca8228
LazTheArtist <kingofcreepersyt@gmail.com>
2021-03-29 20:49:01 +0200

    but its server side now

--- src/main/java/multiteam/arcadia/setup/EventHandler.java
+++ src/main/java/multiteam/arcadia/setup/EventHandler.java
@@ -13,8 +13,8 @@ public class EventHandler {
 
     @SubscribeEvent
     public static void onLightningStrike(EntityJoinWorldEvent event){
-        if(event.getEntity().getType() == EntityType.LIGHTNING_BOLT){
-            System.out.println("A Lightning Struck and it was detected!!!");
+        if(event.getEntity().getType() == EntityType.LIGHTNING_BOLT && !event.getWorld().isClientSide){
+            System.out.println("A Lightning Struck and it was detected on the server only!!!");
         }
     }
 }
