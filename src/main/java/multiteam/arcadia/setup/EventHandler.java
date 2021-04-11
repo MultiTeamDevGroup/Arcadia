47199f4e9ed22ecd853647077101bc8e6e35b411
LazTheArtist <kingofcreepersyt@gmail.com>
2021-04-11 13:08:46 +0200

    when you fall down from the cloud realm, now it puts you back into the overworld

--- src/main/java/multiteam/arcadia/setup/EventHandler.java
+++ src/main/java/multiteam/arcadia/setup/EventHandler.java
@@ -3,9 +3,18 @@ package multiteam.arcadia.setup;
 import multiteam.arcadia.ArcadiaMod;
 import multiteam.arcadia.setup.entity.ModEntitys;
 import multiteam.arcadia.setup.entity.zap.ZapEntity;
+import multiteam.arcadia.setup.util.TeleportationTools;
+import multiteam.arcadia.setup.world.dimension.ModDimensions;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.event.world.NoteBlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -23,6 +32,24 @@ public class EventHandler {
                 Entity zapEnty = new ZapEntity(ModEntitys.ZAP.get(),event.getWorld());
                 event.getWorld().addFreshEntity(zapEnty);
                 zapEnty.setPos(event.getEntity().getX()+ThreadLocalRandom.current().nextInt(2, 5 + 1), event.getEntity().getY()+ThreadLocalRandom.current().nextInt(2, 5 + 1), event.getEntity().getZ()+ThreadLocalRandom.current().nextInt(2, 5 + 1));
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void onPlayerFallFromDimension(TickEvent.PlayerTickEvent event){
+        if(event.player instanceof PlayerEntity){
+            PlayerEntity playerEntity = (PlayerEntity) event.player;
+            if(playerEntity.level.dimension() == ModDimensions.CLOUD_REALM && playerEntity.position().y < -1 ){
+                ServerPlayerEntity serverPlayer = null;
+                if(playerEntity instanceof ServerPlayerEntity){
+                    serverPlayer = (ServerPlayerEntity)playerEntity;
+                }
+                if(serverPlayer !=null){
+                    BlockPos currentPlayerPos = serverPlayer.blockPosition();
+                    ServerWorld world = serverPlayer.getServer().getLevel(World.OVERWORLD);
+                    TeleportationTools.teleport(serverPlayer, world, new BlockPos(currentPlayerPos.getX(), 260, currentPlayerPos.getZ()));
+                }
             }
         }
     }
